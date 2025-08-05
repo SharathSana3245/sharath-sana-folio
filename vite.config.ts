8b5458ccbe879f36712945472254ce1660295c78
Sharath Kumar Sana (ALLYIS INC) <v-ssana@microsoft.com>
2025-08-05 16:50:10 +0530

    change2

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Replace this with your actual repo name
+const repoName = 'sharath-sana-folio';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
+
   server: {
     host: "::",
     port: 8080,
